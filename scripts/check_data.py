f7206cd08fe886bf44bf2f091b2d4d4365a9f5f3
workualemu <alemu.worku@gmail.com>
2025-08-10 22:55:44 -0400

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,7 @@
 from sdg.open_sdg import open_sdg_check
 
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_data: scripts/alter_data.py)
 
 # If everything was valid, perform the build.
 if not validation_successful:

cdae9073b2626ab6a02b5f1dfb42b0f0675f8010
workualemu <alemu.worku@gmail.com>
2025-08-11 09:47:38 -0400

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,5 +1,5 @@
 from sdg.open_sdg import open_sdg_build
-from scripts.alter_data import alter_data
+from alter_data import alter_data
 
 open_sdg_build(config='config_data.yml',
     alter_data=alter_data,)
